6a639bff6e20b2bcb1290ea72508a8f5da2fb601
Syafitra Alfa Ridzi <ia1alfaridzi@gmail.com>
2023-01-22 00:54:31 +0700

    feat(report): add report function and view

---
+++ resources/views/live_report.blade.php
@@ -0,0 +1,59 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Product Detail Page</title>
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+    </head>
+    <body>
+        <div class="container">
+            <div class="row">
+                <div class="col-md-12">
+                    <h1>{{ $laundries->name}}</h1>
+                </div>
+                <div class="col-md-12">
+                    <p>{{ $laundries->customer_name}}</p>
+                    <p>{{ $laundries->customer_phone_number}}</p>
+                    <p>{{ $laundries->price}} IDR</p>
+                    <p><b>{{ $laundries->customer_address}}</b></p>
+                </div>
+                @foreach ($laundries->cloths as $cloth )
+                <div class="col-md-12">
+                    <div class="col-md-6">
+                        <img class="img-responsive" src="{{ $cloth-> image}}" alt="Product Image">
+                    </div>
+                    <div class="col-md-6">
+                        <h3>{{ $cloth->category}}</h3>
+                        <p>{{ $cloth->status}}</p>
+                        <p>{{ $cloth->detail}}</p>
+                    </div>
+                </div>
+                @endforeach
+
+            </div>
+        </div>
+    </body>
+</html>
+
+
+{{-- @foreach($laundries as $laundry) --}}
+    <div class="card shadow">
+        <div class="card-header">
+            <div class="row align-items-center">
+                <div class="col">
+                    <h2 class="mb-0">{{ $laundries->laundry_id}}</h2>
+                </div>
+            </div>
+        </div>
+        <div class="card-body border-0">
+            <div class="row">
+                <div class="col-4">
+                    @foreach ($laundries->cloths as $cloth )
+
+                    @endforeach
+                    <img src="/storage/product/{{ $cloth -> detail }}" width= "300px" >
+                </div>
+            </div>
+        </div>
+    </div>
+    <br>
+{{-- @endforeach --}}
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\LaundryController;
 
 /*
 |--------------------------------------------------------------------------
@@ -16,3 +17,5 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/report/{report}', [LaundryController::class, 'liveReport']);
