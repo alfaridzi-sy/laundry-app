cbafeef41453e69a1c2775a8813af21eb6f70c81
Syafitra Alfa Ridzi <ia1alfaridzi@gmail.com>
2023-01-22 00:47:58 +0700

    fix(Cloth): fix cloth image upload

--- app/Http/Controllers/ClothController.php
+++ app/Http/Controllers/ClothController.php
@@ -4,11 +4,30 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Models\Cloth;
+use Illuminate\Support\Facades\Storage;
+use Illuminate\Support\Facades\Validator;
+
 
 class ClothController extends Controller
 {
     public function addCloth(Request $request)
     {
+        $validator = Validator::make($request->all(), [
+            'image' => 'required|image:jpeg,png,jpg,gif,svg|max:2048'
+        ]);
+        if ($validator->fails()) {
+            return response()->json([
+                'status' => 500,
+                'error' => 'SYSTEM_ERROR',
+                'data' => $validator->messages()->first(),
+            ], 500);
+            return sendCustomResponse($validator->messages()->first(),  'error', 500);
+        }
+
+        if ($request->hasFile('image')) {
+            $image = $request->image->getClientOriginalName();
+            $request->image->storeAs('cloth', $image, 'public');
+        }
         $cloth = Cloth::find($request->post('cloth_id'));
 
         if(!$cloth){
@@ -16,7 +35,7 @@ class ClothController extends Controller
                 'cloth_id' => $request->post('cloth_id'),
                 'detail' => $request->post('detail'),
                 'category' => $request->post('category'),
-                'image' => $request->post('image'),
+                'image' => $image,
                 'status' => $request->post('status'),
             ]);
 
@@ -30,7 +49,7 @@ class ClothController extends Controller
                 'cloth_id' => $request->post('cloth_id'),
                 'detail' => $request->post('detail'),
                 'category' => $request->post('category'),
-                'image' => $request->post('image'),
+                'image' => $image,
                 'status' => $request->post('status'),
             ]);
 
